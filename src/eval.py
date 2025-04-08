fc1b2f3ee83e57cc37861fb5117bc0556392c268
joris <joris.gabrisch@web.de>
2025-04-08 21:20:52 +0200

    saved the results

--- src/eval.py
+++ src/eval.py
@@ -8,17 +8,22 @@ FILES = ["20000_500_high_reward_positive.json", "40000_500_high_reward_positive.
 PATH = "src/results/"
 
 def average_json_array(files : list[str]) -> None:
-     results = {}
+        results = []
 
-     for file_name in files:
+        for file_name in files:
 
-          with open(Path(PATH + file_name), "r") as file:
-              data = json.load(file)
-          keys = data[0].keys()
-          averaged_dict = {key: statistics.mean(d[key] for d in data) for key in keys}
+            with open(Path(PATH + file_name), "r") as file:
+                data = json.load(file)
+            keys = data[0].keys()
+            averaged_dict = {key: statistics.mean(d[key] for d in data) for key in keys}
 
-          print(f"for the file : {file_name} : ")
-          print(averaged_dict)
-          print("--------------------------")
+            print(f"for the file : {file_name} : ")
+            print(averaged_dict)
+            print("--------------------------")
+            results.append(averaged_dict)
+        
+        with open(Path(PATH + "aggregated_results.json"), "w") as file:
+            json.dump(results, file, indent=4)  
+
 
 average_json_array(files=FILES)
